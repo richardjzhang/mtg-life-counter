20c49e42bf76e65359b997e66f11afe3c9a4d52f
richardjzhang <rzhang@mathspace.com.au>
2020-01-04 13:33:37 +1100

    Update font style

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 
-import App from "./App";
-import "./index.css";
+import App from './App';
+import './index.css';
 
-const rootElement = document.getElementById("root");
+WebFont.load({
+  google: {
+    families: ['Acme:400,500,600,700', 'sans-serif']
+  }
+});
+
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
